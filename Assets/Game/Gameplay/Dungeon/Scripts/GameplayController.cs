ed32a860db2790f619c4690e5f4e55bf8032dbc5
Matías Lorenzo <matete@gmail.com>
2025-06-06 02:20:20 -0300

    Fix: ui cursor handling

--- Assets/Game/Gameplay/Dungeon/Scripts/GameplayController.cs
+++ Assets/Game/Gameplay/Dungeon/Scripts/GameplayController.cs
@@ -2,69 +2,69 @@ using UnityEngine;
 
 public class GameplayController : MonoBehaviour
 {
-    [SerializeField] private PlayerController playerController = null;
-    [SerializeField] private GameplayUI gameplayUI = null;
-    [SerializeField] private WinZone winZone = null;
-    [SerializeField] private AudioEvent musicEvent = null;
-    [SerializeField] private AudioEvent winEvent = null;
-    [SerializeField] private AudioEvent loseEvent = null;
+  [SerializeField] private PlayerController playerController = null;
+  [SerializeField] private GameplayUI gameplayUI = null;
+  [SerializeField] private WinZone winZone = null;
+  [SerializeField] private AudioEvent musicEvent = null;
+  [SerializeField] private AudioEvent winEvent = null;
+  [SerializeField] private AudioEvent loseEvent = null;
 
-    private void Start()
-    {
-        playerController.Init(ToggleOnPause, gameplayUI.UpdatePlayerHealth, LoseGame);
-        gameplayUI.Init(ToggleTimeScale, ToggleOffPause);
-        winZone?.Init(VictoryPlayer, WinGame);
+  private void Start()
+  {
+    playerController.Init(ToggleOnPause, gameplayUI.UpdatePlayerHealth, LoseGame);
+    gameplayUI.Init(ToggleTimeScale, ToggleOffPause);
+    winZone?.Init(VictoryPlayer, WinGame);
 
-        GameManager.Instance.AudioManager.PlayAudio(musicEvent);
+    GameManager.Instance.AudioManager.PlayAudio(musicEvent);
 
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+  }
 
-    private void ToggleOnPause()
-    {
-        gameplayUI.TogglePause(true);
-        ToggleTimeScale(false);
-    }
+  private void ToggleOnPause()
+  {
+    gameplayUI.TogglePause(true);
+    ToggleTimeScale(false);
+  }
 
-    private void ToggleOffPause()
-    {
-        ToggleTimeScale(true);
-        playerController.TogglePause(false);
-    }
+  private void ToggleOffPause()
+  {
+    ToggleTimeScale(true);
+    playerController.TogglePause(false);
+  }
 
-    private void VictoryPlayer()
-    {
-        playerController.DisableInput();
-        playerController.PlayVictoryAnimation();
+  private void VictoryPlayer()
+  {
+    playerController.DisableInput();
+    playerController.PlayVictoryAnimation();
 
-        EnemyManager.Instance.OnPlayerVictory();
-        GameManager.Instance.AudioManager.StopCurrentMusic(
-            onSuccess: () =>
-            {
-                GameManager.Instance.AudioManager.PlayAudio(winEvent);
-            });
-    }
+    EnemyManager.Instance.OnPlayerVictory();
+    GameManager.Instance.AudioManager.StopCurrentMusic(
+        onSuccess: () =>
+        {
+          GameManager.Instance.AudioManager.PlayAudio(winEvent);
+        });
+  }
 
-    private void LoseGame()
-    {
-        gameplayUI.OpenLosePanel();
-        EnemyManager.Instance.OnPlayerDefeated();
+  private void LoseGame()
+  {
+    gameplayUI.OpenLosePanel();
+    EnemyManager.Instance.OnPlayerDefeated();
 
-        GameManager.Instance.AudioManager.StopCurrentMusic(
-            onSuccess: () =>
-            {
-                GameManager.Instance.AudioManager.PlayAudio(loseEvent);
-            });
-    }
+    GameManager.Instance.AudioManager.StopCurrentMusic(
+        onSuccess: () =>
+        {
+          GameManager.Instance.AudioManager.PlayAudio(loseEvent);
+        });
+  }
 
-    private void WinGame()
-    {
-        gameplayUI.OpenWinPanel();
-    }
+  private void WinGame()
+  {
+    gameplayUI.OpenWinPanel();
+  }
 
-    private void ToggleTimeScale(bool status)
-    {
-        Time.timeScale = status ? 1f : 0f;
-    }
+  private void ToggleTimeScale(bool status)
+  {
+    Time.timeScale = status ? 1f : 0f;
+  }
 }
--- Assets/Game/Gameplay/Dungeon/Scripts/UI/GameplayUI.cs
+++ Assets/Game/Gameplay/Dungeon/Scripts/UI/GameplayUI.cs
@@ -5,92 +5,120 @@ using UnityEngine.UI;
 
 public class GameplayUI : MonoBehaviour
 {
-    [Header("HUD Settings")]
-    [SerializeField] private Slider playerHealthSlider = null;
+  [Header("HUD Settings")]
+  [SerializeField] private Slider playerHealthSlider = null;
 
-    [Header("Pause Settings")]
-    [SerializeField] private GameObject pausePanel = null;
-    [SerializeField] private Button resumeBtn = null;
-    [SerializeField] private Button backToMenuBtn = null;
+  [Header("Pause Settings")]
+  [SerializeField] private GameObject pausePanel = null;
+  [SerializeField] private Button resumeBtn = null;
+  [SerializeField] private Button backToMenuBtn = null;
 
-    [Header("Lose Settings")]
-    [SerializeField] private GameObject losePanel = null;
-    [SerializeField] private Button retryBtn = null;
-    [SerializeField] private Button loseBackToMenuBtn = null;
+  [Header("Lose Settings")]
+  [SerializeField] private GameObject losePanel = null;
+  [SerializeField] private Button retryBtn = null;
+  [SerializeField] private Button loseBackToMenuBtn = null;
 
-    [Header("Win Settings")]
-    [SerializeField] private GameObject winPanel = null;
-    [SerializeField] private Button winBackToMenuBtn = null;
+  [Header("Win Settings")]
+  [SerializeField] private GameObject winPanel = null;
+  [SerializeField] private Button winBackToMenuBtn = null;
 
-    private Action<bool> onToggleTimeScale = null;
-    private Action onToggleOnPlayerInput = null;
+  private Action<bool> onToggleTimeScale = null;
+  private Action onToggleOnPlayerInput = null;
 
-    private void Start()
+  private void Start()
+  {
+    resumeBtn.onClick.AddListener(() => TogglePause(false));
+    backToMenuBtn.onClick.AddListener(BackToMenu);
+
+    retryBtn.onClick.AddListener(Retry);
+    loseBackToMenuBtn.onClick.AddListener(BackToMenu);
+
+    winBackToMenuBtn.onClick.AddListener(BackToMenu);
+  }
+
+  public void Init(Action<bool> onToggleTimeScale, Action onToggleOnPlayerInput)
+  {
+    this.onToggleTimeScale = onToggleTimeScale;
+    this.onToggleOnPlayerInput = onToggleOnPlayerInput;
+  }
+
+  public void TogglePause(bool status)
+  {
+    ToggleCursor(status);
+    Time.timeScale = status ? 0f : 1f;
+    pausePanel.SetActive(status);
+    onToggleTimeScale?.Invoke(!status);
+
+    if (!status)
     {
-        resumeBtn.onClick.AddListener(() => TogglePause(false));
-        backToMenuBtn.onClick.AddListener(BackToMenu);
-
-        retryBtn.onClick.AddListener(Retry);
-        loseBackToMenuBtn.onClick.AddListener(BackToMenu);
-
-        winBackToMenuBtn.onClick.AddListener(BackToMenu);
+      onToggleOnPlayerInput?.Invoke();
     }
+  }
 
-    public void Init(Action<bool> onToggleTimeScale, Action onToggleOnPlayerInput)
+  public void OpenLosePanel()
+  {
+    ToggleCursor(true);
+    losePanel.SetActive(true);
+  }
+
+  public void OpenWinPanel()
+  {
+    ToggleCursor(true);
+    winPanel.SetActive(true);
+  }
+
+  public void UpdatePlayerHealth(int currentLives, int maxLives)
+  {
+    playerHealthSlider.value = (float)currentLives / maxLives;
+  }
+
+  private void BackToMenu()
+  {
+    resumeBtn.interactable = false;
+    backToMenuBtn.interactable = false;
+
+    retryBtn.interactable = false;
+    loseBackToMenuBtn.interactable = false;
+
+    winBackToMenuBtn.interactable = false;
+
+    GameManager.Instance.ChangeScene(SceneGame.Menu);
+    GameManager.Instance.AudioManager.ToggleMusic(true);
+    onToggleTimeScale?.Invoke(true);
+  }
+
+  private void Retry()
+  {
+    retryBtn.interactable = false;
+    loseBackToMenuBtn.interactable = false;
+
+    GameManager.Instance.ChangeScene(SceneGame.Gameplay);
+    GameManager.Instance.AudioManager.ToggleMusic(true);
+  }
+
+  void ToggleCursor(bool status)
+  {
+    if (status)
     {
-        this.onToggleTimeScale = onToggleTimeScale;
-        this.onToggleOnPlayerInput = onToggleOnPlayerInput;
+      EnableCursor();
     }
-
-    public void TogglePause(bool status)
+    else
     {
-        pausePanel.SetActive(status);
-        onToggleTimeScale?.Invoke(!status);
-
-        if (!status)
-        {
-            onToggleOnPlayerInput?.Invoke();
-        }
+      DisableCursor();
     }
+  }
 
-    public void OpenLosePanel()
-    {
-        losePanel.SetActive(true);
-    }
 
-    public void OpenWinPanel()
-    {
-        winPanel.SetActive(true);
-    }
+  void DisableCursor()
+  {
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+  }
 
-    public void UpdatePlayerHealth(int currentLives, int maxLives)
-    {
-        playerHealthSlider.value = (float)currentLives / maxLives;
-    }
+  void EnableCursor()
+  {
+    Cursor.lockState = CursorLockMode.None;
+    Cursor.visible = true;
+  }
 
-    private void BackToMenu()
-    {
-        resumeBtn.interactable = false;
-        backToMenuBtn.interactable = false;
-
-        retryBtn.interactable = false;
-        loseBackToMenuBtn.interactable = false;
-
-        winBackToMenuBtn.interactable = false;
-
-        GameManager.Instance.ChangeScene(SceneGame.Menu);
-        GameManager.Instance.AudioManager.ToggleMusic(true);
-        onToggleTimeScale?.Invoke(true);
-    }
-
-    private void Retry()
-    {
-        retryBtn.interactable = false;
-        loseBackToMenuBtn.interactable = false;
-
-        GameManager.Instance.ChangeScene(SceneGame.Gameplay);
-        GameManager.Instance.AudioManager.ToggleMusic(true);
-    }
-
-    
 }
